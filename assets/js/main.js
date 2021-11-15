fc4c7477e6cf6a37d45a6bb20667567b266103be
Ilya Shutikov <DeliasZen@gmail.com>
2021-11-15 13:27:28 +0300

    init repo

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+	$('.header-navbar__btn').on('click', function () {
+		$(this).toggleClass('active');
+		$('.header-navbar__list').stop(true,true).slideToggle(500);
+	});
+});
